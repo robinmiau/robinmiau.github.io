b5e6be99e54e4909e817af63bf46e681c21dd8c9
Robin <robin@robinmiau.de>
2025-10-29 22:35:22 +0100

    chore: initialize project with Vite configuration and package.json

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  base: './',
+});
